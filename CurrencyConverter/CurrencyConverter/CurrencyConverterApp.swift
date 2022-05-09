fc8b10e575e11c00c7fac95c274bd3d601b51b21
Pho (Tran An) Vu / Haemin <phovu0703@gmail.com>
2022-05-09 01:29:10 -0400

    Add files via upload

---
+++ CurrencyConverter/CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Pho Vu on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CurrencyConverter/CurrencyConverter/Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Utils.swift
+//  QRCodeScanner
+//
+//  Created by Pho Vu on 5/8/22.
+//
+
+import Foundation
+
+extension String {
+    static let numberFormatter = NumberFormatter()
+    var doubleValue: Double {
+        String.numberFormatter.decimalSeparator = "."
+        if let result = String.numberFormatter.number(from: self) {
+            return result.doubleValue
+        } else {
+            String.numberFormatter.decimalSeparator = ","
+            if let result = String.numberFormatter.number(from: self) {
+                return result.doubleValue
+            }
+        }
+        return 0
+    }
+}
